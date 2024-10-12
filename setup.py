e4a28dbe3647fb6f968501b90cf84aac855ace0e
Happy Sharma <happycse54@gmail.com>
2024-10-12 16:38:50 +0530

    added pre-commit

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages, setup
 
+
 def get_requirements(file_path: str) -> list[str]:
     "This function will return the list of packages"
     ...
     requirements = []
     # import pdb; pdb.set_trace()
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-    
-    requirements = [requirement.replace("\n", "") for requirement in requirements]
+        requirements = file_obj.readlines()
+
+    requirements = [
+        requirement.replace("\n", "") for requirement in requirements
+    ]  # noqa
     if "-e ." in requirements:
         requirements.remove("-e .")
     return requirements
@@ -21,5 +24,5 @@ setup(
     author="Happy Kumar Sharma",
     author_email="happycse54@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+    install_requires=get_requirements("requirements.txt"),
 )
